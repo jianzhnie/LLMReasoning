d848cffb02818990c52f834f3ed4cd7f1e695bb0
jianzhnie <jianzhnie@126.com>
2025-10-29 15:46:02 +0800

    Update llmreasoning

---
+++ run.sh
@@ -0,0 +1,10 @@
+# gitee
+git config --global user.name 'jianzhnie' 
+git config --global user.email 'jianzhnie@126.com'
+
+source set_env.sh
+
+# pre-commit
+pre-commit run --file llmreasoning/data_process/*
+
+
---
+++ set_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+install_path=/home/jianzhnie/llmtuner/Ascend/CANN8.2.RC1
+source $install_path/ascend-toolkit/set_env.sh
+source $install_path/nnal/atb/set_env.sh
+
+# source /home/jianzhnie/llmtuner/software/miniconda3/bin/activate vllm091
+source /home/jianzhnie/llmtuner/software/miniconda3/bin/activate rlhf
